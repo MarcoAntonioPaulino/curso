c97e59267f85b87cc3f168cb34c0f340232c0ffa
Marco Antonio Paulino Pérez <iceteamdevelopers@gmail.com>
2020-11-24 12:25:43 -0600

    se agrego el metodo validarCadena para verificar que las cadenas de texto no este vacias o que solo contengan espacios en blanco

--- src/main/java/com/mindfirelabs/curso/util/GestionStrings.java
+++ src/main/java/com/mindfirelabs/curso/util/GestionStrings.java
@@ -1,5 +1,15 @@
 package com.mindfirelabs.curso.util;
 
 public class GestionStrings {
-
+    
+    public String validarCadena(String cadena) {
+        if(cadena.isEmpty()) {
+            return "El campo no puede estar vacio";
+        }
+        if(cadena.trim().isEmpty()) {
+            return "No se permiten solo espacios en blanco";
+        }
+        return null;
+    }
+    
 }
