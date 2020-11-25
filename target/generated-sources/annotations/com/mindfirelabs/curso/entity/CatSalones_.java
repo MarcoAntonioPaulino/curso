825fda7acfe7ab0730220d31f3488cfd2b9b7636
Marco Antonio Paulino Pérez <iceteamdevelopers@gmail.com>
2020-11-25 11:17:23 -0600

    cambios en el tema de la aplicacion

--- target/generated-sources/annotations/com/mindfirelabs/curso/entity/CatSalones_.java
+++ target/generated-sources/annotations/com/mindfirelabs/curso/entity/CatSalones_.java
@@ -5,7 +5,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.7.7.v20200504-rNA", date="2020-11-24T12:05:00")
+@Generated(value="EclipseLink-2.7.7.v20200504-rNA", date="2020-11-25T11:15:49")
 @StaticMetamodel(CatSalones.class)
 public class CatSalones_ { 
 
